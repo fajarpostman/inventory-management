3d82e2c216f105b1b36e8fdb4ec4cd35a083c67e
fajarpostman <fajardwirianto3@gmail.com>
2023-02-26 01:52:45 +0700

    update roles controller and route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Roles\RolesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(RolesController::class)->group(function() {
+    Route::post('store', 'storeRoles');
 });
+
+// Route::group(['prefix' => 'roles', 'as' => 'roles'], function() {
+//     Route:post('store', [Roles::class, 'storeRoles']);
+// });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
